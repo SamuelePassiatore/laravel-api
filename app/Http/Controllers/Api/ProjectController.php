d04a22ec4358629847d0daf9d0692e1e596d148a
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-15 15:23:26 +0100

    added logic in project controller index with response json

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -12,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::where('is_public', true)->with('type', 'technologies')->orderBy('updated_at', 'DESC')->get();
+        return response()->json($projects);
     }
 
     /**
